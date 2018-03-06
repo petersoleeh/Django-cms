079c1f3bd88621de39a3066e160a155e355352e4
Game Time <gametimeke@gmail.com>
2018-03-06 07:55:31 +0000

    uninstall djangocms-link 2.1.2

--- settings.py
+++ settings.py
@@ -11,7 +11,6 @@ INSTALLED_ADDONS = [
     'djangocms-file',
     'djangocms-googlemap',
     'djangocms-history',
-    'djangocms-link',
     'djangocms-picture',
     'djangocms-snippet',
     'djangocms-style',
