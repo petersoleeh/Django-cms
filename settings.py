dc7e7babb9ab5d6858232a47516efeb920854e59
Game Time <gametimeke@gmail.com>
2018-03-06 07:50:43 +0000

    install djangocms-googlemap 1.1.0

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ INSTALLED_ADDONS = [
     'aldryn-addons',
     'aldryn-django',
     'aldryn-sso',
+    'djangocms-googlemap',
     'djangocms-link',
     'djangocms-snippet',
     'djangocms-text-ckeditor',
